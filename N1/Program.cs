e67d07da40d426bc6da1c4786f11f71091d1e93c
lolmem745 <93839006+lolmem745@users.noreply.github.com>
2022-11-19 02:13:26 +0300

    Init commit

---
+++ N1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Сколько чисел хоите ввести?");
+int arrLength = Convert.ToInt32(Console.ReadLine()), counter = 0;
+int[] arr = new int[arrLength];
+Console.WriteLine("Введите числа через Enter.");
+for (int i = 0; i < arrLength; i++)
+{
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+    if (arr[i] > 0) counter++;
+}
+Console.WriteLine($"Чисел больше нуля: {counter}");
---
+++ N2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x;
+double y;
+
+if (k1 != k2)
+{
+    x = (b2 - b1) / (k1 - k2);
+    y = (k1 * x) + b1;
+    Console.Write($"Точка пересечения: ({x},{y})");
+}
+else if (b1 == b2) Console.Write("Прямые совпадают.");
+else Console.Write("Прямые параллельны.");
